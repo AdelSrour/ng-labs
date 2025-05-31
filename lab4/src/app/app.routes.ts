68a231a0c037c57f4707c6207fc29106287e196b
Adel Srour <adelmalys@hotmail.com>
2025-06-01 01:34:26 +0300

    Add cart component

--- lab4/src/app/app.routes.ts
+++ lab4/src/app/app.routes.ts
@@ -7,5 +7,6 @@ export const routes: Routes = [
     {path: "product/:id", loadComponent: ()=> import ('./product/product.component').then(p => p.ProductComponent), canActivate:[authGuard]},
     { path: "login", loadComponent: () => import('./login/login.component').then(p => p.LoginComponent) },
     { path: "register", loadComponent: () => import('./register/register.component').then(p => p.RegisterComponent) },
+    { path: "cart", loadComponent: () => import('./cart/cart.component').then(p => p.CartComponent) },
     { path: "**", loadComponent: () => import('./notfound/notfound.component').then(p => p.NotfoundComponent) },
 ];
