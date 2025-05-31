0fbdab4e9820a3056ef7286d4a642125bbc10ec4
Adel Srour <adelmalys@hotmail.com>
2025-06-01 01:37:29 +0300

    Edit imports in register page

--- lab4/src/app/register/register.component.ts
+++ lab4/src/app/register/register.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {  RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
