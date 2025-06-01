adcb867bb9a8b6773461de3c34d4005971dcb822
Adel Srour <adelmalys@hotmail.com>
2025-06-02 00:29:51 +0300

    Update login

--- lab4/src/app/login/login.component.ts
+++ lab4/src/app/login/login.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule,ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required)
+  })
 
+  login(userForm: NgForm) {
+    console.log(userForm.valid);
+  }
 }
--- lab4/src/app/register/register.component.ts
+++ lab4/src/app/register/register.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import {  RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterModule],
+  imports: [RouterModule,FormsModule,ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  signupForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    repassword: new FormControl('', [Validators.required])
+  });
 
+  signup() {
+    console.log(this.signupForm);
+    
+  }
 }
